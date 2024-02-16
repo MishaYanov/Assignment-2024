511bb2a46437cb1bef62a312c239583b33f8f46d
MishaYanov <86347940+MishaYanov@users.noreply.github.com>
2024-02-16 23:08:53 +0200

    nest config for root

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { PublisherModule } from './publisher/publisher.module';
 import { DomainModule } from './domain/domain.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     PublisherModule,
     DomainModule,
     PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes the config globally available
+    })
   ],
   controllers: [],
   providers: [],
